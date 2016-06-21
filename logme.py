f7fb2f750f7f2007193b5f8f851690aa84a8c428
David Gwartney <davidg@boundary.com>
2016-06-21 16:09:00 -0700

    Check for existing database earlier to make sure schema gets applied if the database is new

--- logme.py
+++ logme.py
@@ -6,6 +6,11 @@ from datetime import datetime
 class LogMe(object):
 
     def __init__(self, path=None):
+        """
+        Initialize logging object and setup schema
+
+        :param path:
+        """
         self._db_path = None
         self._db_schema_path = 'schema.sql'
         self._conn = None
@@ -16,32 +21,52 @@ class LogMe(object):
             self._db_path = 'measurement.db'
 
         self._db_schema_path = 'schema.sql'
+        db_is_new = not os.path.exists(self._db_path)
+
         self.open_database()
 
-        db_is_new = not os.path.exists(self._db_path)
         if db_is_new:
             self._create_schema()
         self.close_database()
 
     def _create_schema(self):
+        """
+        Create the measurement schema in the database
+        :return:
+        """
         with open(self._db_schema_path, 'rt') as f:
             schema = f.read()
             self._conn.executescript(schema)
 
     def open_database(self):
+        """
+        Open a connection to the database
+        :return:
+        """
         if self._conn is None:
             self._conn = sqlite3.connect(self._db_path)
 
     def close_database(self):
+        """
+        Close the connection to the database
+        :return:
+        """
         if self._conn is not None:
             self._conn.close()
             self._conn = None
 
     def log(self, metric, value, source, timestamp):
+        """
+        Log a row to the database
+        :param metric:
+        :param value:
+        :param source:
+        :param timestamp:
+        :return:
+        """
         self.open_database()
         sql = "insert into measurement(metric, value, source, timestamp) values('{0}', {1}, '{2}', '{3}');".format(
             metric, value, source, timestamp)
-        print(sql)
         self._conn.executescript(sql)
 
 
